fe5a00fbcffbdc5bf9120d37312966bc65e5fae2
Neveh Allon <NevehAllon@gmail.com>
2020-11-12 23:04:17 +0200

    fix: drag, drop and sort bugs

--- client/js/main.js
+++ client/js/main.js
@@ -3,6 +3,8 @@ import { getWordTrieStr, getPossibleWords, getWordValues } from "./getRequests.j
 import gridState from "./createGrid.js";
 
 let lettersUsed = 0;
+let isZoomed = false;
+let fired = false;
 
 const bag = _.shuffle(_.shuffle(letters));
 let rivalRack = [];
@@ -38,7 +40,10 @@ $("#startGame").click(startGame);
 function setDraggable(x) {
   x.draggable({
     connectToSortable: "#rack",
+    scroll: false,
     revert: "invalid",
+    zIndex: 100,
+    stack: ".tile",
     start: function (event, ui) {
       ui.helper.data("rejected", false);
       ui.helper.data("original-position", ui.helper.offset());
@@ -48,6 +53,11 @@ function setDraggable(x) {
         if (ui.helper.data("rejected") === true) {
           ui.helper.offset(ui.helper.data("original-position"));
         }
+
+      $("#rack .tile").css({ left: "0", top: "0" });
+
+      removeDuplicates();
+
       console.count(); //repaint Game/Grid State here
     },
   });
@@ -57,14 +67,21 @@ function resetSortable() {
   $("#rack").sortable({
     placeholder: "ph-class",
     scroll: false,
+    tolerance: "intersect",
     revert: 200,
+    out: function (ev, ui) {
+      fired = false;
+    },
+    over: function (event, ui) {
+      fired = false;
+    },
     receive: function (event, ui) {
+      fired = false;
       $(ui.item)
         .css({
           height: "45px",
           width: "42px",
           "font-size": "x-large",
-          "z-index": "11",
         })
         .children("div")
         .css({
@@ -81,46 +98,61 @@ $(".column").droppable({
   accept: ".tile",
   tolerance: "intersect",
   drop: function (ev, ui) {
+    if (!fired) {
+      fired = true;
+      return;
+    }
+
     if ($(this).children(".tile").length > 0) {
       ui.helper.data("rejected", true);
       return false;
     }
 
-    let tileClone = $(ui.draggable).clone().detach();
-    $(this).prepend(tileClone);
+    let tileClone = $(ui.draggable);
+    $(this).append(tileClone);
 
-    $(ui.draggable).remove();
-    setTimeout(function () {
-      $(ui.draggable).remove();
-    }, 5);
-
-    tileClone.draggable({
-      connectToSortable: "#rack",
-    });
+    // $(ui.draggable).remove();
+    // setTimeout(function () {
+    //   $(ui.draggable).remove();
+    // }, 5);
+    removeDuplicates();
 
     setDraggable(tileClone);
-    $(tileClone)
-      .css({
-        position: "relative",
-        left: "0",
-        top: "0",
-        height: "25.5px",
-        width: "25.5px",
-        "font-size": "15px",
-        "z-index": "11",
-      })
-      .children("div")
-      .css({
-        left: "15px",
-        bottom: "10px",
-        "font-weight": "600",
-        padding: "1px",
-      });
-
-    $(".tile").each((i) => {
-      if ($(`[data-drag="${i}"]`).length > 1) {
-        $(`[data-drag="${i}"]`)[0].remove();
-      }
+    $(tileClone).css({
+      position: "relative",
+      left: "0",
+      top: "0",
+      // height: "53px",
+      // width: "53px",
+      // "font-size": "15px",
     });
+    // .children("div")
+    // .css({
+    //   left: "15px",
+    //   bottom: "10px",
+    //   "font-weight": "600",
+    //   padding: "1px",
+    // });
+
+    if (!isZoomed) {
+      $("#board").css({
+        height: "705px",
+        width: "640px",
+        "grid-template-rows": "repeat(15, 47px)",
+        "grid-template-columns": "repeat(15, 44px)",
+        "justify-content": "safe center",
+        margin: "0",
+      });
+      isZoomed = true;
+    }
+    tileClone[0].scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
   },
 });
+
+function removeDuplicates() {
+  $(".tile").each((i) => {
+    if ($(`[data-drag="${i}"]`).length > 1) {
+      $(`[data-drag="${i}"]`).slice(0, -1).remove();
+    }
+  });
+}
